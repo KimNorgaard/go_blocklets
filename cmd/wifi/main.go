3d37b35ca20746716f9d15517cb6d27fa97e9fae
Kim Nørgaard <jasen@jasen.dk>
2018-02-02 22:07:22 +0100

    move stuff

--- cmd/wifi/main.go
+++ cmd/wifi/main.go
@@ -4,27 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"os"
 	"strconv"
 	"strings"
+
+	gobl "github.com/KimNorgaard/go_blocklets"
 )
-
-func getGreenToRed(pct int) string {
-	var r, g float64
-
-	pctFloat := float64(pct)
-
-	if pctFloat < 50 {
-		r = 255
-		g = math.Floor((pctFloat * 2) * 255 / 100)
-	} else {
-		r = math.Floor(255 - (pctFloat*2-100)*255/100)
-		g = 255
-	}
-
-	return fmt.Sprintf("#%02X%02X%02X", int(r), int(g), 0)
-}
 
 func wifi(wlIf string) (int, error) {
 	// wifi quality
@@ -99,7 +84,7 @@ func main() {
 		output = wlIf
 		colorText = "#FF0000"
 	} else {
-		colorText = getGreenToRed(quality)
+		colorText = gobl.getGreenToRed(quality)
 	}
 
 	fullText = output
